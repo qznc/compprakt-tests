28aa9db46608369cee0e2bd8dcfd37dbd41ae9f2
Andreas Zwinkau <zwinkau@kit.edu>
2016-11-03 10:25:52 +0100

    Refactor

--- testsuite.py
+++ testsuite.py
@@ -15,58 +15,37 @@ def setup_argparser(argparser, default_env):
     )
 test.suite.add_argparser_setup(setup_argparser)
 
-def step_lextest(env):
-    """Command to execute as lextest step"""
-    return execute(env, "%(compiler)s --lextest %(testname)s" % env, timeout=60)
+def generic_step(param):
+    def step(env):
+        """Command to execute as step"""
+        cmd = "%(compiler)s --"+param+" %(testname)s"
+        return execute(env, cmd % env, timeout=60)
+    return step
 
-def lex_test(name):
-    """How performing a lexer test is actually done"""
-    t = Test(name)
-    # Sisyphus supports multiple steps like "compile" and "run".
-    # Here we need only one.
-    # Check return code and output against reference file.
-    t.add_step("lextest", step_lextest, checks=[
-        check_retcode_zero,
-        create_check_reference_output(name+".ref"),
-    ])
-    return t
+def generic_test(stepname, stepcmd, check_output=True):
+    def create_test(name):
+        t = Test(name)
+        checks = [check_retcode_zero]
+        if check_output:
+            checks.append(create_check_reference_output(name+".ref"))
+        # Sisyphus supports multiple steps like "compile" and "run".
+        # Here we need only one.
+        # Check return code and output against reference file.
+        t.add_step(stepname, generic_step(stepcmd), checks=checks)
+        return t
+    return create_test
+
+lex_test = generic_test("lex", "lextest")
+parse_test = generic_test("parse", "parsetest", check_output=False)
+ast_test = generic_test("ast", "print-ast")
+semantic_test = generic_test("semantic", "check", check_output=False)
 
 lex_testfiles = [p for p in glob("lexer/*") if not p.endswith(".ref")]
 test.suite.make("lex", tests=map(lex_test, lex_testfiles))
 
-def step_parsetest(env):
-    """Command to execute as parsetest step"""
-    return execute(env, "%(compiler)s --parsetest %(testname)s" % env, timeout=60)
-
-def step_asttest(env):
-    """Command to execute as AST parsetest step"""
-    return execute(env, "%(compiler)s --print-ast %(testname)s" % env, timeout=60)
-
-def parse_test(name):
-    t = Test(name)
-    t.add_step("parsetest", step_parsetest, checks=[ check_retcode_zero ])
-    return t
-
-def ast_test(name):
-    t = Test(name)
-    t.add_step("AST test", step_asttest, checks=[
-        check_retcode_zero,
-        create_check_reference_output(name+".ref"),
-    ])
-    return t
-
 parse_testfiles = [p for p in glob("parser/*") if not p.endswith(".ref")]
 test.suite.make("parse", tests=map(parse_test, parse_testfiles))
 test.suite.make("ast", tests=map(ast_test, parse_testfiles))
-
-def step_semantictest(env):
-    """Command to execute as semantictest step"""
-    return execute(env, "%(compiler)s --check %(testname)s" % env, timeout=60)
-
-def semantic_test(name):
-    t = Test(name)
-    t.add_step("semantictest", step_semantictest, checks=[ check_retcode_zero ])
-    return t
 
 semantic_testfiles = [p for p in glob("semantic/*.java")]
 test.suite.make("semantic", tests=map(semantic_test, semantic_testfiles))
